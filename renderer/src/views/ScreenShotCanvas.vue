7cf4dd3faa12795f3a9cfb6d1ff54de80ab47dc0
shixinghao <245978782@qq.com>
2023-07-18 16:37:17 +0800

    chore

---
+++ renderer/src/views/ScreenShotCanvas.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script setup lang="ts">
+/**
+ * 替换 canvas 实现截屏
+ * 1. 主线程读取 screen 媒体信息，转化为 image
+ * 2. 将 image 转化为 canvas 作为页面背景（窗口置顶）
+ * 3. 
+ */
+import { reactive, ref, computed, onBeforeMount } from "vue";
+</script>
+
+<style scoped lang="less"></style>
